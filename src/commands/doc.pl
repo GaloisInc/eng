fe0ec445646729298caddf631723a9f058e31be7
Kevin Quick <kq1quick@gmail.com>
2025-03-11 10:53:16 -0700

    Add doc show subcommand

--- src/commands/doc.pl
+++ src/commands/doc.pl
@@ -1,10 +1,12 @@
 :- module(doc, [ doc_cmd/3, doc_focus/1, doc_help/1, doc_help/2 ]).
 
 :- use_module(library(apply)).
+:- use_module(library(filesex)).
 :- use_module(library(lists)).
 :- use_module(library(strings)).
 :- use_module('../englib').
 :- use_module('../lando_tool').
+:- use_module('exec_subcmds').
 
 
 doc_focus("Documentation Engineering").
@@ -34,6 +36,7 @@ doc_help(Info) :-
 
 doc_help(list, "List the set of known documents").
 doc_help(info, "Get summary information for the specified document").
+doc_help(show, "Display the document via pager.").
 doc_help(lando, "Run Lando requirements engineering operations").
 
 
@@ -43,7 +46,7 @@ doc_cmd(_, [list|_Args], 0) :- writeln('Known documents:'), show_doc(_) ; true.
 
 doc_cmd(_, [info], 1) :- print_message(error, specify_doc_id).
 doc_cmd(_, [info,Doc|_Args], 0) :- ( doc_info(Doc), !
-                                   ; writeln('Unable to find that document.')
+                                   ; print_message(error, doc_not_found(Doc))
                                    ).
 
 doc_cmd(_, [lando], 1) :- writeln({|string||
@@ -51,6 +54,21 @@ doc_cmd(_, [lando], 1) :- writeln({|string||
 |}
                        ).
 doc_cmd(_, [lando,SrcFile|LandoOp], Sts) :- lando(SrcFile, LandoOp, Sts).
+
+doc_cmd(_, [show], 1) :- print_message(error, specify_doc_id).
+doc_cmd(Context, [show,Doc|_Args], Sts) :-
+    eng:eng(doc, Doc, location, Loc),
+    !,
+    directory_file_path(DocDir, DocFile, Loc),
+    format(atom(InDir), "{TopDir}/~w", DocDir),
+    format(atom(Cmd), 'less ~w~n', DocFile),
+    do_exec(Context, "Document display", {}, [Cmd], [], InDir, Sts).
+doc_cmd(_, [show,Doc|_Args], 1) :-
+    eng:key(doc, Doc),
+    !,
+    print_message(error, no_doc_loc(Doc)).
+doc_cmd(_, [show,Doc|_Args], 1) :-
+    print_message(error, doc_not_found(Doc)).
 
 doc_cmd(_, [Cmd|_], 1) :-
     print_message(error, invalid_doc_subcmd(Cmd)).
@@ -105,3 +123,7 @@ prolog:message(invalid_doc_subcmd(Cmd)) -->
     [ 'Valid sub-commands: ~w~n' - [ CS ] ].
 prolog:message(specify_doc_id) -->
     [ 'Please specify the document to show information for (see `eng doc list`)~n' - [] ].
+prolog:message(no_doc_loc(Doc)) -->
+    [ 'No location provided for document ~w~n' - [Doc] ].
+prolog:message(doc_not_found(Doc)) -->
+    [ 'Unknown document: ~w~n' - [Doc] ].
